3521c4545b5d62249ed2133a3d1e0f24241378b3
Antoine James Tournepiche <antoinejt.serveur@gmail.com>
2021-06-15 15:07:23 +0200

    Partiel ex 3

---
+++ Partiel/Ex3/Application.java
@@ -0,0 +1,11 @@
+import java.util.Arrays;
+import java.util.List;
+
+public class Application {
+    public static void main(String[] args) {
+        List<String> list = Arrays.asList("Salut", "Bonjour", "Hello", "au revoir", "ciao");
+        StringUtils.allMatches(list, str -> str.length() >= 6).forEach(System.out::println);
+        StringUtils.allMatches(list, str -> str.length() % 2 == 1).forEach(System.out::println);
+        StringUtils.transformedList(list, String::toUpperCase).forEach(System.out::println);
+    }
+}
---
+++ Partiel/Ex3/StringUtils.java
@@ -0,0 +1,14 @@
+import java.util.List;
+import java.util.function.Function;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+
+public class StringUtils {
+    public static List<String> allMatches(List<String> candidates, Predicate<String> matchFunction) {
+        return candidates.stream().filter(matchFunction).collect(Collectors.toList());
+    }
+
+    public static List<String> transformedList(List<String> originals, Function<String, String> transformer) {
+        return originals.stream().map(transformer).collect(Collectors.toList());
+    }
+}
